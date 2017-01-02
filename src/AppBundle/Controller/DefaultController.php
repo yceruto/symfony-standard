b52946d39423ca16b70d8347471d324afaf0f969
Yonel Ceruto <yonelceruto@gmail.com>
2017-01-01 19:05:23 -0500

    choice value sample

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -13,9 +14,21 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $form = $this->createFormBuilder()
+            ->add('foo', 'Symfony\Component\Form\Extension\Core\Type\ChoiceType', [
+                'choices' => ['A', 'B', 'C'],
+                'choice_value' => function ($value) {
+                    if (null === $value) {
+                        throw new \InvalidArgumentException('Nullable value is unexpected here!');
+                    }
+
+                    return $value;
+                },
+            ])
+            ->getForm();
+
         return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'form' => $form->createView(),
         ));
     }
 }
